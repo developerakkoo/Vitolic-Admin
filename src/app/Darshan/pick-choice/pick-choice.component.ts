62eed51a9796cf258040aa1791d6b1fa2833e5f5
varsha1113 <varshaguha43@gmail.com>
2022-07-29 18:57:57 +0530

    Varsha Ui

--- src/app/Darshan/pick-choice/pick-choice.component.ts
+++ src/app/Darshan/pick-choice/pick-choice.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-pick-choice',
   templateUrl: './pick-choice.component.html',
